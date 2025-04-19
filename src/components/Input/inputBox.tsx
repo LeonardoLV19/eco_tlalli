023055f94ed63089ab8643d88fe7bdb336251c50
YaelLopezS <a01664809@tec.mx>
2025-04-19 14:34:06 -0600

    Actualización InputBox
    Se implementa el icono y un estilo mas apegado al figma del proyecto

--- src/components/Input/inputBox.tsx
+++ src/components/Input/inputBox.tsx
@@ -1,22 +1,32 @@
 "use client";
 
 import { Input } from "../ui/input";
+import PersonIcon from '@mui/icons-material/Person';
 
 const InputBox = () => {
   return (
-    <div className="flex flex-col items-center m-5">
+    <div className="flex flex-col items-start m-5">
       <label
         htmlFor="input-box"
-        className="text-lg font-bold mb-2 text-gray-700"
+        className="mb-2"
+        style={{ fontSize: "16px", color: "#525252", fontFamily: "'Alkatra', sans-serif" }}
       >
-        Enter your text:
+        Nombre en la tarjeta
       </label>
-      <Input
-        id="input-box"
-        type="text"
-        placeholder="Type here..."
-        className="px-4 py-2 border-2 border-green-500 rounded-md w-[300px] text-base focus:border-green-700 transition-colors"
-      />
+      <div className="relative w-[300px]">
+        <PersonIcon 
+          className="absolute right-3 top-1/2 transform -translate-y-1/2" 
+          fontSize="medium"
+          style={{ color: "#78B689" }}
+        />
+        <Input
+          id="input-box"
+          type="text"
+          placeholder="Escribe aquí"
+          className="pr-10 pl-4 py-2 border-2 rounded-md w-full text-base"
+          style={{ fontFamily: "'Alkatra', sans-serif" }}
+        />
+      </div>
     </div>
   );
 };
