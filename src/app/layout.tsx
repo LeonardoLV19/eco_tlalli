6247e3742de6414dc839496010cd4a1ea263e4e8
MarcoMilos <marcomiloslavich03@gmail.com>
2025-05-11 22:17:28 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body
         className={`antialiased`}
       >
-        <TabBar role="vendedor" />
+        <TabBar role="admin" />
         {children}
       </body>
     </html>
