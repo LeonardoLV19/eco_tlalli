9dbd40b4c73a008c5d2e77de7225195a2ef36d4a
YaelLopezS <a01664809@tec.mx>
2025-05-13 19:59:34 -0600

    Actualización de LogIn

--- src/components/Pill/Pill.tsx
+++ src/components/Pill/Pill.tsx
@@ -41,7 +41,7 @@ const Pill = ({ text, color, onClick }: PillProps) => {
 
   return (
     <button onClick={onClick} className={cn(
-        "rounded-full px-4 py-1 text-sm font-sans inline-flex items-center gap-2",
+        "rounded-full px-4 py-1 text-sm font-sans inline-flex items-center gap-2 justify-center text-center",
         colorMap[color]
       )}>
       {color === "volver" && <ArrowBackIcon fontSize="large" />}
